597309206f2765e97ade5f402415a5a14b80f188
Satria-WS <extencezone02@gmail.com>
2023-10-20 10:59:19 +0700

    fixing case min-max

--- easy/7-minMax.js
+++ easy/7-minMax.js
@@ -1,5 +1,3 @@
-const  value = [1,2,3,4,5];
-
 function miniMaxSum(arr) {
   // Write your code here
   const minValue = Math.min(...arr);
@@ -13,22 +11,41 @@ function miniMaxSum(arr) {
   let maxSum = 0;
   
   for (let i = 0 ; i < arr.length ; i ++) {
-      if(arr[i] !== minValue) {
-          minArr.push(arr[i])
-          maxSum += arr[i];
+    
+    if(minValue !== maxValue) {
+     if(arr[i] !== minValue) {
+        minArr.push(arr[i])
+        maxSum += arr[i];
       }
       
-      if(arr[i] !== maxValue) {
-        maxArr.push(arr[i]);
-        minSum += arr[i];
+    if(arr[i] !== maxValue) {
+      maxArr.push(arr[i]);
+      minSum += arr[i];
       }
+    } else {
+      minArr.push(arr[i])
+      maxArr.push(arr[i])
+      
+      maxSum += arr[i] - 1
+      minSum += arr[i] - 1
+      
+    }
+    
+
+    
   }
   
   
   console.log(minSum,maxSum);
-  // console.log(minArr , maxArr)
+  console.log(minArr , maxArr)
+  
+  console.log(minValue)
+   console.log(maxValue)
  
   
 
 }
+
+const duplicate = [5, 5, 5, 5, 5];
+const value = [1,2,3,4,5]
 miniMaxSum(value)

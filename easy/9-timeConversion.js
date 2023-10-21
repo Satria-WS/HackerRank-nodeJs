c7108f8417c81bc6efe2801ce97cb4d6cff6ed71
Satria-WS <extencezone02@gmail.com>
2023-10-21 12:14:26 +0700

    add

--- easy/9-timeConversion.js
+++ easy/9-timeConversion.js
@@ -2,27 +2,74 @@ function timeConversion(s) {
   // Write your code here
   const time = s;
 
-  const hours = time.slice(0,2)
-  const minutes = time.slice(3,5);
-  const day = time.slice(8,10);
+  let hours = parseInt(time.slice(0,2))
+  let minutes = time.slice(3,5);
+  let second = time.slice(6 , 8)
+  let day = time.slice(8,10);
+  
+  
+  let checkingDay = time.includes(day)
+  
 
-  // for (let i = 0; i < time.length; i++){
+  
+  if (day.toUpperCase().includes('PM')) {
+    if ( hours !== 12 ) {
+       hours = (hours + 12).toString().padStart(2,'0'); 
+      console.log(true , "PM")
+    }
+    
+    
+
+  } else if( day.toUpperCase().includes('AM') ) {
+    if( hours !== 12 ) {
+    hours = (hours + 0).toString().padStart(2,'0'); 
+    } else {
+     hours = (hours - 12).toString().padStart(2,'0'); 
+    }
+  
+  
+    console.log(true , "AM")
+  } else {
+    console.log("invalid input")
+  }
+  
+  
+  console.log(typeof (2 + 3).toString().padStart(2, '0')); 
+
+  
+   let originalTime = `${hours}:${minutes}:${second}${day}`
+    
+    
+
    
-  // }
-  return {hours,minutes,day}
+  
+  return {
+    time :{hours,minutes,second ,day},
+    originalTime: originalTime
+    
+  }
 
 }
 
-const s ="07:05:45"
+const s ="01:01:45AM"
 const result = timeConversion(s);
 console.log(result);
 
-let text = "Hello world!";
-console.log(text[0])
+var str = "Hello THERE."; 
+var check1 = str.includes("there"); 
+console.log(check1)
 
 
-// const time = "07:05:45PM"
-// const timeSlice = time.slice(8,10);
-// const timeX = time.indexOf(timeSlice);
-// const day = timeX ?  "AM" : "PM"
-// console.log(day)
+//ALGORITMA
+
+// if pm -> +12
+// if am -> -12
+
+// if (day === PM   ){
+//   if( hours > 12) {
+//      hours += 0
+//      day = ""
+//}
+// } else if (day === AM) {
+//   
+// }

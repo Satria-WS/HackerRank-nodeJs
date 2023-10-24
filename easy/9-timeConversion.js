68d3841cbe43ee9bff68cebca238c32b2db99f62
Satria-WS <extencezone02@gmail.com>
2023-10-24 12:52:04 +0700

    add time

--- easy/9-timeConversion.js
+++ easy/9-timeConversion.js
@@ -2,63 +2,38 @@ function timeConversion(s) {
   // Write your code here
   const time = s;
 
-  let hours = parseInt(time.slice(0,2))
-  let minutes = time.slice(3,5);
-  let second = time.slice(6 , 8)
-  let day = time.slice(8,10);
-  
-  
-  let checkingDay = time.includes(day)
-  
+  let hours = parseInt(time.slice(0, 2));
+  let minutes = time.slice(3, 5);
+  let second = time.slice(6, 8);
+  let day = time.slice(8, 10);
 
-  
-  if (day.toUpperCase().includes('PM')) {
-    if ( hours !== 12 ) {
-       hours = (hours + 12).toString().padStart(2,'0'); 
-      console.log(true , "PM")
+  if (day.toUpperCase().includes("PM")) {
+    if (hours !== 12) {
+      hours = (hours + 12).toString().padStart(2, "0");
+      console.log(true, "PM");
     }
-    
-    
-
-  } else if( day.toUpperCase().includes('AM') ) {
-    if( hours !== 12 ) {
-    hours = (hours + 0).toString().padStart(2,'0'); 
+  } else if (day.toUpperCase().includes("AM")) {
+    if (hours !== 12) {
+      hours = (hours + 0).toString().padStart(2, "0");
     } else {
-     hours = (hours - 12).toString().padStart(2,'0'); 
+      hours = (hours - 12).toString().padStart(2, "0");
     }
-  
-  
-    console.log(true , "AM")
   } else {
-    console.log("invalid input")
+    console.log("invalid input");
   }
-  
-  
-  console.log(typeof (2 + 3).toString().padStart(2, '0')); 
 
-  
-   let originalTime = `${hours}:${minutes}:${second}${day}`
-    
-    
+  let originalTime = `${hours}:${minutes}:${second}${day}`;
 
-   
-  
   return {
-    time :{hours,minutes,second ,day},
-    originalTime: originalTime
-    
-  }
-
+    time: { hours, minutes, second, day },
+    originalTime: originalTime,
+  };
 }
 
-const s ="01:01:45AM"
+
+const s = "01:01:45AM";
 const result = timeConversion(s);
 console.log(result);
-
-var str = "Hello THERE."; 
-var check1 = str.includes("there"); 
-console.log(check1)
-
 
 //ALGORITMA
 
@@ -71,5 +46,5 @@ console.log(check1)
 //      day = ""
 //}
 // } else if (day === AM) {
-//   
+//
 // }
